fe4a07ef371b9646eff07bf502ba822a67d39029
googool <wachmaninoff@gmail.com>
2023-01-23 00:05:31 +0900

    setup

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Realtime Chat with SocketIO, express and Nodejs
 
 # To Do
 
-- [ ] Server
-- [ ] Pug
-- [ ] Static
-- [ ] SocketIO
-- [ ] Chat
-- [ ] login/logout
+기능
+[ ] login >> id입력으로 대체
+[ ] add friend >> cookie에 저장하는 방식으로(채팅은 저장 x)
 
+페이지
+[ ] intro 페이지
+[ ] login 페이지
+[ ] friends 페이지
+[ ] chats 페이지
+[ ] chat 페이지
+
+
+
+>> 일단 페이지들 디자인이랑 같이 가져오고 채팅방 기능 만들고 채팅방 꾸미기
--- src/server.js
+++ src/server.js
@@ -1,32 +1,16 @@
 import express from "express";
-import { join } from "path";
-import socketIO from "socket.io";
 
-const PORT = 4000;
+const PORT = 3000;
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", join(__dirname, "views"));
-app.use(express.static(join(__dirname, "static")));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListening = () =>
-  console.log(`Server running: http://localhost:${PORT}!!!`);
+  console.log(`Server running: http://localhost:${PORT}`);
 
-const server = app.listen(PORT, handleListening);
-
-const io = socketIO(server);
-
-let sockets = [];
-
-io.on("connection", (socket) => {
-  socket.on("newMessage", ({message}) => {
-    socket.broadcast.emit("messageNotif", { 
-      message,
-      nickname: socket.nickname || "anonymous",
-    });
-  });
-  socket.on("setNickname", ({nickname}) => {
-    socket.nickname = nickname;
-  });
-});
+app.listen(PORT, handleListening);

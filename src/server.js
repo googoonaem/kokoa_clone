4b1222a7e03068089640c0987d77607bdcbaff3e
googool <wachmaninoff@gmail.com>
2023-12-17 01:04:23 +0900

    css done
    겉모양은 끝냄 이제 내용물을 만들자

--- src/server.js
+++ src/server.js
@@ -7,11 +7,32 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/friends", (req, res) => res.render("friends"));
-app.get("/chats", (req, res) => res.render("chats"));
-app.get("/find", (req, res) => res.render("find"));
-app.get("/more", (req, res) => res.render("more"));
+app.get("/", (req, res) => res.render("home", {
+  pageTitle: 'home'
+}));
+app.get("/friends", (req, res) => res.render("friends", {
+  pageTitle: 'friends'
+}));
+app.get("/chats", (req, res) => res.render("chats", {
+  pageTitle: 'chats'
+}));
+app.get("/chat/:nickName", (req, res) => {
+  const nickName = req.params.nickName;
+  console.log(nickName);
+  res.render("chat", {
+    pageTitle: 'chat',
+    nickName 
+  })}
+);
+app.get("/find", (req, res) => res.render("find", {
+  pageTitle: 'find'
+}));
+app.get("/more", (req, res) => res.render("more", {
+  pageTitle: 'more'
+}));
+app.get("/setting", (req, res) => res.render("setting", {
+  pageTitle: 'setting'
+}));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListening = () =>

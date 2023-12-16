27455429ff5f63f7765d3750ae0750bb68b3f7dc
googool <wachmaninoff@gmail.com>
2023-12-16 23:11:51 +0900

    whatisallthis

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/friends", (req, res) => res.render("friends"));
+app.get("/chats", (req, res) => res.render("chats"));
+app.get("/find", (req, res) => res.render("find"));
+app.get("/more", (req, res) => res.render("more"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListening = () =>
